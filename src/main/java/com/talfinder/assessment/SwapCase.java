2e2aed9f7bcf54934625e61c4e12eb0fa624403a
Shashidhar G A <shashidhar.a@hebbalelabs.com>
2019-02-21 21:11:16 +0530

    Inital commit

---
+++ src/main/java/com/talfinder/assessment/SwapCase.java
@@ -0,0 +1,16 @@
+package com.talfinder.assessment;
+
+public class SwapCase {
+
+  public static String doSwapCase(String input) {
+    String output = "";
+    return output;
+  }
+
+  public static void main(String[] args) {
+    String input = "TalFinder";
+    String expectedOutput = "tALfINDER";
+    String actualOutput = doSwapCase(input);
+    System.out.println(expectedOutput.equals(actualOutput));
+  }
+}
